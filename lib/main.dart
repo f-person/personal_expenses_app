d9c6e44903adffd580b2530147e9b68189dcb1ff
f-person <agakaryan.arshak@gmail.com>
2019-07-02 15:34:06 +0400

    Add transactions model. Add some yet hardcoded transactions.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(PersonalExpensesApp());
 
 class PersonalExpensesApp extends StatelessWidget {
@@ -13,15 +15,45 @@ class PersonalExpensesApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'an ice cream',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'a bread',
+      amount: 12,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Personal Expenses'),
       ),
-      body: Center(
-        child: Text('Home Page'),
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('CHART'),
+                color: Colors.blue,
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Text(tx.title);
+              }).toList(),
+            )
+          ]),
     );
   }
 }
