920d5ce52608b6f9f7aa1ff5509b6b99c678b261
f-person <agakaryan.arshak@gmail.com>
2019-07-02 19:12:35 +0400

    Add submitData method

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
   final Function addTransaction;
-  NewTransaction(this.addTransaction);
 
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  NewTransaction(this.addTransaction);
+
+  void submitData() {
+    String enteredTitle = titleController.text;
+    double enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTransaction(enteredTitle, enteredAmount);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,18 +31,20 @@ class NewTransaction extends StatelessWidget {
               cursorColor: Colors.purple,
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
               cursorColor: Colors.green,
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
             ),
             FlatButton(
               child: Text('Add Transaction'),
               textColor: Colors.purple,
-              onPressed: () => addTransaction(
-                  titleController.text, double.parse(amountController.text)),
-            ),
+              onPressed: submitData,
+            )
           ],
         ),
       ),
