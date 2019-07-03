c69e99776580dd2eb249958939558f5d69a48152
f-person <agakaryan.arshak@gmail.com>
2019-07-03 23:47:10 +0400

    Add date and time pickers

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
 class NewTransaction extends StatefulWidget {
   final Function addTransaction;
 
@@ -10,13 +12,13 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
-  final amountController = TextEditingController();
-
-  void submitData() {
-    String enteredTitle = titleController.text;
-    double enteredAmount = double.parse(amountController.text);
+  void _submitData() {
+    String enteredTitle = _titleController.text;
+    double enteredAmount = double.parse(_amountController.text);
 
     if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return;
@@ -25,6 +27,62 @@ class _NewTransactionState extends State<NewTransaction> {
     widget.addTransaction(enteredTitle, enteredAmount);
 
     Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2019),
+        lastDate: DateTime.now(),
+        builder: (BuildContext context, Widget child) {
+          return FittedBox(
+            child: Theme(
+              child: child,
+              data: ThemeData(
+                primaryColor: Colors.purple[400],
+              ),
+            ),
+          );
+        }).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      _selectedDate = pickedDate;
+
+      showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.now(),
+        builder: (BuildContext context, Widget child) {
+          return FittedBox(
+            child: Theme(
+              child: child,
+              data: ThemeData(
+                primaryColor: Colors.purple[400],
+                accentColor: Theme.of(context).accentColor,
+              ),
+            ),
+          );
+        },
+      ).then((pickedTime) {
+        if (pickedTime == null) {
+          setState(() {
+            _selectedDate = DateTime(
+                _selectedDate.year,
+                _selectedDate.month,
+                _selectedDate.day,
+                TimeOfDay.now().hour,
+                TimeOfDay.now().minute);
+          });
+        } else {
+          setState(() {
+            _selectedDate = DateTime(_selectedDate.year, _selectedDate.month,
+                _selectedDate.day, pickedTime.hour, pickedTime.minute);
+          });
+        }
+      });
+    });
   }
 
   @override
@@ -38,26 +96,28 @@ class _NewTransactionState extends State<NewTransaction> {
             TextField(
               cursorColor: Colors.purple,
               decoration: InputDecoration(labelText: 'Title'),
-              controller: titleController,
-              onSubmitted: (_) => submitData(),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
             ),
             TextField(
               cursorColor: Colors.green,
               decoration: InputDecoration(labelText: 'Amount'),
-              controller: amountController,
+              controller: _amountController,
               keyboardType: TextInputType.number,
-              onSubmitted: (_) => submitData(),
+              onSubmitted: (_) => _submitData(),
             ),
             Container(
               height: 70.0,
               child: Row(
                 children: <Widget>[
-                  Text('No Date Chosen!'),
+                  Text(_selectedDate == null
+                      ? 'No Date Chosen!'
+                      : DateFormat('d.M.y H:m').format(_selectedDate)),
                   FlatButton(
                     textColor: Theme.of(context).primaryColor,
                     child: Text('Choose Date!',
                         style: TextStyle(fontWeight: FontWeight.bold)),
-                    onPressed: () {},
+                    onPressed: _presentDatePicker,
                   )
                 ],
               ),
@@ -68,7 +128,7 @@ class _NewTransactionState extends State<NewTransaction> {
                 child: Text('Add Transaction'),
                 color: Theme.of(context).primaryColor,
                 textColor: Theme.of(context).textTheme.button.color,
-                onPressed: submitData,
+                onPressed: _submitData,
               ),
             )
           ],
